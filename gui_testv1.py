05b71a7a5629ca1a46d40f53374b492d6a9b2264
tmonster1138 <terry.neeley@gmail.com>
2017-08-08 19:37:22 +0000

    Final Update

--- gui_testv1.py
+++ gui_testv1.py
@@ -4,7 +4,7 @@ def say_my_name():
 def change_text_size(slider_value):
     welcome_message.font_size(slider_value)
 app = App(title="Hello world")
-welcome_message = Text(app, text="Welcome to my app", size=26, font="Helvetica", color="red")
+welcome_message = Text(app, text="Welcome to my app", size=28, font="Helvetica", color="red")
 update_text = PushButton(app, command=say_my_name, text="Display my name")
 my_name = TextBox(app, width=20)
 text_size = Slider(app, command=change_text_size, start=10, end=80)
