f8501b1ed0f9c38af85cc2984720dd31c6b69e31
tmonster1138 <terry.neeley@gmail.com>
2017-08-08 18:59:00 +0000

    2nd Update

--- gui_testv1.py
+++ gui_testv1.py
@@ -1,5 +1,8 @@
-from guizero import App, Text, TextBox
+from guizero import App, Text, TextBox, PushButton
+def say_my_name():
+    welcome_message.set( my_name.get() )
 app = App(title="Hello world")
 welcome_message = Text(app, text="Welcome to my app", size=26, font="Helvetica", color="red")
+update_text = PushButton(app, command=say_my_name, text="Display my name")
 my_name = TextBox(app, width=20)
 app.display()
